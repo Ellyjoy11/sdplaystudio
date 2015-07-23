f42be33deec6941c77550cb9c8e355e89ffb5b38
Ellyjoy11 <ellyjoy11@gmail.com>
2015-07-23 12:08:59 -0500

    patterns for userdata made case insensitive

--- app/src/main/java/com/elena/sdplay/MainActivity.java
+++ app/src/main/java/com/elena/sdplay/MainActivity.java
@@ -112,6 +112,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 		PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
 
